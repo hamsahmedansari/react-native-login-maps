5d3e5090a8875d1adab949b90c6c8b05f0566341
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-18 15:14:57 +0500

    Update: :construction: firebase update  1 is Modified :  1 : src/utils/firebase.js  1 is Created :  1 : .watchmanconfi

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,15 +2,7 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyCi2s9iijA32T8PIS1TLafPp7_A2_B2nzY",
-  authDomain: "react-native-login-map.firebaseapp.com",
-  databaseURL: "https://react-native-login-map.firebaseio.com",
-  projectId: "react-native-login-map",
-  storageBucket: "react-native-login-map.appspot.com",
-  messagingSenderId: "39486846337",
-  appId: "1:39486846337:web:67835ac07759fafb"
-};
+const config = {};
 firebase.initializeApp(config);
 // export const initializeApp = () => ;
 
